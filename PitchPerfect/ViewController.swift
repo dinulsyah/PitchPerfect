927ef23c824bd2927459a927e715558b3310d200
arif dinulsyahputra <arif.putra@agenkan.co.id>
2020-06-03 20:30:00 +0700

    Add View Controller and Navigation Controller

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
-
+    @IBAction func recordAudio(_ sender: UIButton) {
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
+    }
 }
-
